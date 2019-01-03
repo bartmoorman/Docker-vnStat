72bfdb87dac40a98871748a62f2239d140080c80
Bart Moorman <github@id10t.us>
2019-01-03 09:55:23 -0700

    Config: create table

--- htdocs/inc/vnstat.class.php
+++ htdocs/inc/vnstat.class.php
@@ -56,6 +56,11 @@ class vnStat {
 
   private function initDb() {
     $query = <<<EOQ
+CREATE TABLE IF NOT EXISTS `config` (
+  `config_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `key` TEXT NOT NULL UNIQUE,
+  `value` TEXT NOT NULL
+);
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `username` TEXT NOT NULL UNIQUE,
